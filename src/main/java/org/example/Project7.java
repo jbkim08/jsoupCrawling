83806d56d39839ef740e1c0945a3bd7ab43394f1
JB <drv98@naver.com>
2024-10-24 11:14:42 +0900

    책정보를 객체로 리턴

--- src/main/java/org/example/Project7.java
+++ src/main/java/org/example/Project7.java
@@ -1,14 +1,26 @@
 package org.example;
 
+import org.example.model.ExelVO;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.Scanner;
 
 public class Project7 {
+
+    static final Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         /* 계속 실행되게! */
         boolean running = true;
         do {
             menu();
-            Scanner scanner = new Scanner(System.in);
             int key = Integer.parseInt(scanner.nextLine());
             switch (key) {
                 case 1:
@@ -40,7 +52,76 @@ public class Project7 {
     }
 
     private static void searchBook() {
-        System.out.println("책을 검색합니다.");
+        System.out.print("책 검색 : ");
+        String query = scanner.nextLine().trim();
+        ExelVO vo = getNaverAPI(query);
+
+    }
+
+    /**
+     * 검색어 입력을 받아 네이버 API 로 책을 검색한 결과 리턴
+     * @param query 검색어
+     * @return 책 객체
+     */
+    private static ExelVO getNaverAPI(String query) {
+
+        try {
+            String openApi = "https://openapi.naver.com/v1/search/book.json?query="
+                    + URLEncoder.encode(query, "UTF-8");
+            URL url = new URL(openApi);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("X-Naver-Client-Id", "Oex1SY46jLWbaoNsZ7RJ");
+            conn.setRequestProperty("X-Naver-Client-Secret", "vcQMXqDaLi");
+
+            //System.out.println("응답 코드 : " + conn.getResponseCode());
+
+            BufferedReader br;
+            if(conn.getResponseCode() == 200) {
+                br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+            } else {
+                br = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+            }
+
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+            System.out.println(sb.toString());
+            br.close();
+            conn.disconnect();
+
+            JSONParser jsonParser = new JSONParser();
+            JSONObject jsonObject = (JSONObject)jsonParser.parse(sb.toString());
+            JSONArray arr = (JSONArray)jsonObject.get("items");
+
+            if(arr.isEmpty()) {
+                System.out.println("검색된 책이 없습니다.");
+                return null;
+            }
+
+            JSONObject book = (JSONObject)arr.get(0); //제이슨배열에서 첫번째 책을 가져옴
+            ExelVO vo = new ExelVO();
+            System.out.print(book.get("title") + "\t");
+            System.out.print(book.get("author") + "\t");
+            System.out.print(book.get("publisher") + "\t");
+            System.out.print(book.get("isbn") + "\t");
+            System.out.print(book.get("pubdate") + "\t");
+            System.out.println();
+            vo.setTitle(book.get("title").toString());
+            vo.setAuthor(book.get("author").toString());
+            vo.setCompany(book.get("publisher").toString());
+            vo.setIsbn(book.get("isbn").toString());
+            vo.setImgurl(book.get("image").toString());
+
+            return vo;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 
     private static void listCheck() {
